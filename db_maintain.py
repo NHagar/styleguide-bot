d20a38466d1072521d072aa96e039b7878f83911
Nicholas Hagar <NHagar@MMC-Nicholas-Hagar.local>
2017-05-15 13:56:07 -0700

    database maintenance script

--- db_maintain.py
+++ db_maintain.py
@@ -1,6 +1,9 @@
 import os
+import pickle
 from urlparse import urlparse
 import psycopg2
+
+styleguide = pickle.load(open('style_guide'))
 
 url = urlparse(os.environ["DATABASE_URL"])
 conn = psycopg2.connect(
@@ -11,3 +14,20 @@ conn = psycopg2.connect(
     port=url.port
 )
 cur = conn.cursor()
+
+def create_table():
+    '''makes table'''
+    cur.execute("CREATE TABLE horace (term varchar, definition varchar);")
+
+def insert_dict(sguide):
+    '''add existing dictionary to table'''
+    terms = list(sguide.keys())
+    definitions = list(sguide.values())
+
+    sgdict = zip(terms, definitions)
+
+    for i in sgdict:
+        cur.execute("INSERT INTO horace VALUES (%s, %s)", [i])
+
+create_table()
+insert_dict(styleguide)
